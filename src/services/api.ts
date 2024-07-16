df873e89a9aa12f0d0ea436b4376024ee20f7699
Heitor DB Renthon <heitor34productions@gmail.com>
2024-07-16 17:45:06 -0300

    novo url backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'https://laughing-wood-92171.pktriot.net'
+    baseURL: 'https://renthon-pizzaiolo-backend.onrender.com'
 })
 
 export {api}
